a48e19dd7e347b2455535cf17b4934f27a04c05f
r <94881672+Rylazius@users.noreply.github.com>
2025-12-27 16:36:12 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
+apt update
+apt install unzip -y
+sudo pip3 install --upgrade cython
+export PYTHONPATH=/usr/local/lib/python3.10/site-packages:$PYTHONPATH
+
 # install vapoursynth
 wget https://github.com/Rylazius/blur-prebuilt-22.04/raw/refs/heads/main/vapoursynth.deb
 dpkg -i vapoursynth.deb
@@ -8,3 +13,13 @@ cd /usr/lib/vapoursynth
 wget -O plugins.tar https://files.catbox.moe/7t1ayo.tar # aka plugins.zip, github doesn't allow me to upload files > 25mb so I decided to tar it and catbox.moe
 tar -xvf plugins.tar
 rm plugins.tar
+
+# download & extract blur
+mkdir blur
+cd blur
+wget https://github.com/Rylazius/blur-prebuilt-22.04/raw/refs/heads/main/Blur-Linux-Release-x64.zip
+unzip Blur-Linux-Release-x64.zip
+tar -xvzf blur-Linux-Release-x64.tar.gz
+rm Blur-Linux-Release-x64.zip
+rm blur-Linux-Release-x64.tar.gz
+echo "Done."
